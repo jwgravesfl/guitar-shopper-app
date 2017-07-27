1a3bf8a0734aba235f0ed4389e2854b3722ed759
msnow90 <mattsnowis@yahoo.com>
2017-07-26 20:56:54 -0400

    fixed guitar rendering test and additional errors

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -1,33 +1,21 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 
 class Sidebar extends Component {
 
-    constructor(props) {
-        super(props)
-    }
+  constructor(props) {
+    super(props)
+  }
 
-    render() {
+  render() {
 
-        const sampleBrands = ['Martin', 'Fender', 'Taylor', 'Gibson']
-        const samplePrices = ['100-200', '200-400', '500-900', '1000+']
-
-        const {guitars} = this.props;
-
-        return (
-            <sidebar>
-                <img src="Rock-Guitar-icon.png" className="logo" />
-                <h3>Search By Brand (these are guitars bro):</h3>
+    return (
+      <sidebar>
+        <img src="Rock-Guitar-icon.png" className="logo" />
+        <h3>Search By Brand (these are guitars bro):</h3>
         <ul className="sidebarUl">
-            {
-                guitars.map(guitar => {
-                    return (
-                        <li>{guitar.model} <span><input type="checkbox" /></span></li>
-                    )
-                })
-            }
           {/*<li>Brand 1 <span><input type="checkbox" /></span></li>
           <li>Brand 2 <span><input type="checkbox" /></span></li>
           <li>Brand 3 <span><input type="checkbox" /></span></li>
@@ -47,24 +35,9 @@ class Sidebar extends Component {
           <li>$400 - $500 <span><input type="checkbox" /></span></li>
           <li>$501+ <span><input type="checkbox" /></span></li>
         </ul>
-            </sidebar>
-        );
-    }
-}
-
-
-const mapStateToProps = (state) => {
-  return {
-    guitars: state.guitars
+      </sidebar>
+    );
   }
 }
 
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     filterGuitars(filterTermsObj) {
-//       dispatch(filterGuitars(filterTermsObj));
-//     }
-//   }
-// }
-
-export default connect(mapStateToProps)(Sidebar);
+export default connect()(Sidebar);
