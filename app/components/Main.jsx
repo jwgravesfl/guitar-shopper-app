e4c45aba7ae3b673971a3dc3322a0eb6926d80b8
msnow90 <mattsnowis@yahoo.com>
2017-07-26 20:25:45 -0400

    removed duplicate stuff

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -2,11 +2,9 @@ import React, { Component } from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import Navbar from './Navbar';
-import store from '../store';
 import {fetchAllGuitars} from '../reducers/guitars'
 import BrandList from './BrandList';
 import store from '../store';
-import {fetchAllGuitars} from '../reducers/guitars'
 import {fetchAllBrands} from '../reducers/brands';
 
 export default class Main extends Component {
@@ -20,7 +18,7 @@ export default class Main extends Component {
     }
     render() {
 
-        
+
         return (
             <div>
                 <Sidebar />
