98dce2655476d83bd5eee1ffb2a44e8433cbe831
Kieran Derfus <kieranderfus@gmail.com>
2017-07-28 19:59:42 -0400

    Functioning Checkbox Filter

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -5,41 +5,42 @@ import { connect } from 'react-redux'
 
 class Sidebar extends Component {
 
-  render() {
 
-    const { brands } = this.props;
+    render() {
+        const { brands } = this.props;
 
-    console.log("WE IN THIS:", brands);
-
-    return (
-      <sidebar>
-        <img src="Rock-Guitar-icon.png" className="logo" />
-        <h3>Search By Brand (these are guitars bro):</h3>
-        <ul className="sidebarUl">
-            {
-                brands[0] && brands.map(brand => {
-                    return <li key={brand.id}>{brand.name}<span><input type="checkbox" /></span></li>
-                })
-            }
-        </ul>
-        <h3>Search By Types:</h3>
-        <ul className="sidebarUl">
-          <li>Electric <span><input type="checkbox" /></span></li>
-          <li>Acoustic <span><input type="checkbox" /></span></li>
-        </ul>
-        <h3>Search By Prices:</h3>
-        <ul className="sidebarUl">
-          <li>$100 - $200 <span><input type="checkbox" /></span></li>
-          <li>$201 - $300 <span><input type="checkbox" /></span></li>
-          <li>$301 - $400 <span><input type="checkbox" /></span></li>
-          <li>$400 - $500 <span><input type="checkbox" /></span></li>
-          <li>$501+ <span><input type="checkbox" /></span></li>
-        </ul>
-      </sidebar>
-    );
-  }
+        return (
+            <sidebar>
+                <img src="Rock-Guitar-icon.png" className="logo"/>
+                <h3>Search By Brand (these are guitars bro):</h3>
+                <ul className="sidebarUl">
+                    {
+                        brands[0] && brands.map(brand => {
+                            return <li key={brand.id}>{brand.name}<span><input
+                                type="checkbox"
+                                onClick={ (evt) => this.props.handleBrandsCheck(evt, brand.id) }
+                            /></span></li>
+                        })
+                    }
+                </ul>
+                <h3>Search By Types:</h3>
+                <ul className="sidebarUl">
+                    <li>Electric <span><input type="checkbox" onClick={ evt => this.props.handleCategoriesCheck(evt, 'Electric') } /></span></li>
+                    <li>Acoustic <span><input type="checkbox" onClick={ evt => this.props.handleCategoriesCheck(evt, 'Acoustic') }/></span></li>
+                </ul>
+                <h3>Search By Prices:</h3>
+                <ul className="sidebarUl">
+                    <li>$100 - $200 <span><input type="checkbox" onClick={ evt => this.props.handlePricesCheck(evt, '100-200') } /></span></li>
+                    <li>$201 - $300 <span><input type="checkbox" onClick={ evt => this.props.handlePricesCheck(evt, '201-300') } /></span></li>
+                    <li>$301 - $400 <span><input type="checkbox" onClick={ evt => this.props.handlePricesCheck(evt, '301-400') } /></span></li>
+                    <li>$400 - $500 <span><input type="checkbox" onClick={ evt => this.props.handlePricesCheck(evt, '401-500') } /></span></li>
+                    <li>$501 + <span><input type="checkbox" onClick={ evt => this.props.handlePricesCheck(evt, '501+') } /></span></li>
+                </ul>
+            </sidebar>
+        );
+    }
 }
 
-const mapState = ({ brands }) => ({ brands });
+const mapState = ({brands, guitars }) => ({brands, guitars });
 
 export default connect(mapState)(Sidebar);
